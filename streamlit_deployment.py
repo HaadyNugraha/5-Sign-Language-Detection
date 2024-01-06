a34fc3931ef24f38f1d1ffe5b7c6a67acaf90407
Haady Nugraha <93778459+skididudy@users.noreply.github.com>
2024-01-06 20:54:38 +0700

    Update and rename streamlit_deployment_LSTM.py to streamlit_deployment.py

--- streamlit_deployment.py
+++ streamlit_deployment.py
@@ -1,13 +1,13 @@
 import streamlit as st
+import cv2
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+from tensorflow import keras
 import mediapipe as mp
 import time
-import cv2
 
 #Loading Model
-model = load_model('action_LSTM.h5')
+model = keras.models.load_model('action_LSTM.h5')
 
 mp_holistic = mp.solutions.holistic
 holistic = mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
@@ -34,7 +34,12 @@ def prob_viz(res, actions, frame, colors):
 
 #Real Time Prediction
 def predict_action(frame):
-    global sequence  # Use the global sequence variable
+    global sequence 
+
+    # Check if the frame is None
+    if frame is None:
+        print("Error: Received None frame from the webcam.")
+        return None, None
 
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     image.flags.writeable = False
@@ -42,7 +47,7 @@ def predict_action(frame):
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    #Extracting Keypoints
+    # Extracting Keypoints
     keypoints = extract_keypoints(results)
     sequence.append(keypoints)
     sequence = sequence[-20:]
@@ -56,33 +61,45 @@ def predict_action(frame):
 
 #For Streamlit
 def main():
-    st.title("Action Recognition using Streamlit")
+    st.title("Pendeteksian Gestur Bahasa Isyarat")
 
     video_feed = st.empty()
 
     cap = cv2.VideoCapture(0)
-    
+
+    if not cap.isOpened():
+        st.error("Error: Couldn't open the webcam.")
+        return
+
+    stop_button_counter = 0  # Initialize the counter variable
+
     while cap.isOpened():
         ret, frame = cap.read()
+
+        if not ret:
+            st.error("Error: Couldn't read frame from the webcam.")
+            break
 
         action, viz_frame = predict_action(frame)
 
         if action:
-            st.write("Recognized Action:", action)
+            st.write("Gestur Terdeteksi:", action)
 
         if isinstance(viz_frame, np.ndarray):
             viz_frame_rgb = cv2.cvtColor(viz_frame, cv2.COLOR_BGR2RGB)
 
             video_feed.image(viz_frame_rgb, channels="RGB", use_column_width=True)
 
-        #Stop Button
-        stop_button_key = "stop_button_" + str(time.time())
+        # Stop Button with a unique key
+        stop_button_counter += 1
+        stop_button_key = f"stop_button_{stop_button_counter}"
 
+        # Check if button is clicked
         if st.button("Stop", key=stop_button_key):
             break
 
     cap.release()
-    st.warning("Streamlit app has been stopped.")
+    st.warning("Proses pendeteksian dihentikan.")
 
 if __name__ == "__main__":
     main()
